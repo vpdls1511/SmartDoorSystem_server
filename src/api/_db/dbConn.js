60df07e0a0c8695a7d2b8eb62b873a1a72839271
nGyu <vpdls1511@gmail.com>
2023-07-05 21:45:57 +0900

    refactor(vpdls1511) : dbConn 수정

---
+++ src/api/_db/dbConn.js
@@ -0,0 +1,45 @@
+const mysql = require('mysql')
+require('dotenv').config();
+
+const db_config = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  multipleStatements: true
+}
+
+let pool = mysql.createPool(db_config)
+
+module.exports = {
+
+  query: async (sql, params) => {
+    return new Promise((resolve, rejected) => {
+      pool.getConnection((err, conn) => {
+        if (err)
+          rejected(err)
+        else
+          conn.query(sql, params, (err, data) => {
+
+            if (err)
+              rejected(err)
+            else
+              resolve(data)
+            console.log('connection is returned..')
+            conn.release()
+          })
+      })
+    })
+  },
+
+  get: async () => {
+    return new Promise((resolve, rejected) => {
+      pool.getConnection((err, conn) => {
+        if (err) rejected(err)
+        else resolve(conn)
+      })
+    })
+  },
+
+}
