46111fc775639193203c9644d6ea806dac673340
nGyu <vpdls1511@gmail.com>
2023-07-05 20:03:38 +0900

    feat(vpdls1511) : 파일 업로드 모듈 추가

---
+++ src/utils/uploads/upload.js
@@ -0,0 +1,29 @@
+const multer = require("multer");
+const moment = require("moment");
+const fs = require("fs");
+const path = require("path");
+
+exports.multerUpload = (category) => {
+
+  const storage = multer.diskStorage({
+    destination: async (request, file, callback) => {
+      const dirPath = `static/${category}/` + moment().format('YYYY/MM/DD')
+
+      const isExists = fs.existsSync(dirPath)
+      if (!isExists) {
+        await fs.mkdirSync(dirPath, {recursive: true}, (err) => {
+          if (err) {
+            return console.error(err)
+          }
+        })
+      }
+      callback(null, dirPath)
+    },
+    filename: async (request, file, callback) => {
+      callback(null, `${Date.now()}` + `${path.extname(file.originalname)}`)
+    }
+  })
+  return multer({
+    storage: storage
+  })
+}
